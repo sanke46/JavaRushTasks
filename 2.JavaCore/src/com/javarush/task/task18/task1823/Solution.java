8ea2212d6e73cdc29e1c23258cf7e042485f2763
sanke46 <shift.of.razum@gmail.com>
2017-02-09 21:20:09 +0300

    Adapters
    5 lessons complete

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task18.task1823;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,14 +12,65 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> names = new ArrayList<>();
+        String fileName;
+        while(!(fileName = reader.readLine()).equals("exit")){
+            names.add(fileName);
+        }
 
+        for(String s : names){
+            new ReadThread(s).start();
+        }
+
+        reader.close();
     }
 
     public static class ReadThread extends Thread {
+        private String fileName ;
+
         public ReadThread(String fileName) {
             //implement constructor body
+            this.fileName = fileName;
         }
         // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+
+                try {
+                    FileInputStream fileIn = new FileInputStream(fileName);
+                    HashMap<Integer, Integer> histMap = new HashMap<Integer, Integer>();
+                    byte[] fileBytes = new byte[fileIn.available()];
+                    int max = 1;
+                    int maxByte = 0;
+
+                    while (fileIn.available() > 0) {
+                        fileIn.read(fileBytes);
+                    }
+
+                    for (int i = 0; i < fileBytes.length; i++) {
+                        int byteName = (int) fileBytes[i];
+                        if (histMap.containsKey(byteName)) {
+
+                            int hit = histMap.get(byteName) + 1;
+                            if (hit > max) {
+                                max = hit;
+                                maxByte = byteName;
+                            }
+                            histMap.put(byteName, hit);
+                        } else {
+                            histMap.put(byteName, 1);
+                        }
+                    }
+                    fileIn.close();
+                    resultMap.put(fileName, maxByte);
+
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
+
+        }
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,79 @@
+package com.javarush.task.task19.task1903;
+
+/* 
+Адаптация нескольких интерфейсов
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA","Ukraine");
+        countries.put("RU","Russia");
+        countries.put("CA","Canada");
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact{
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data){
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return this.data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(this.data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return this.data.getContactLastName() + ", " + this.data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String m = String.format("+%d(%2$s)%3$s-%4$s-%5$s",data.getCountryPhoneCode(),
+                    String.format("%010d", data.getPhoneNumber()).substring(0, 3),
+                    String.format("%010d", data.getPhoneNumber()).substring(3, 6),
+                    String.format("%010d", data.getPhoneNumber()).substring(6, 8),
+                    String.format("%010d", data.getPhoneNumber()).substring(8));
+
+            return m;
+        }
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //example UA
+
+        String getCompany();            //example JavaRush Ltd.
+
+        String getContactFirstName();   //example Ivan
+
+        String getContactLastName();    //example Ivanov
+
+        int getCountryPhoneCode();      //example 38
+
+        int getPhoneNumber();           //example 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}
