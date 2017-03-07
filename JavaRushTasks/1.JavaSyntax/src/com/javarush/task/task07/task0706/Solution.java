225679d312923a6f74b63f4ce7989df4571c09ba
sanke46 <shift.of.razum@gmail.com>
2017-03-07 22:56:26 +0300

    Code 07.03.17

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.task07.task0706;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Улицы и дома
@@ -11,5 +14,33 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        int[] arr = new int[15];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfBuildings = 15;
+        int evenNumber = 0;
+        int oddNumber = 0;
+
+        // put number rooms in HashMap
+        for (int i = 0; i < numberOfBuildings; i++) {
+            int x = Integer.parseInt(reader.readLine());
+            arr[i] = x;
+        }
+        reader.close();
+
+        // even and odd
+        for(int i = 0; i < numberOfBuildings; i++){
+            if(i == 0){
+                evenNumber += arr[i];
+            } else if(i % 2 == 0){
+                evenNumber += arr[i];
+            } else {
+                oddNumber += arr[i];
+            }
+        }
+
+        //who is win ?
+        String odd = "В домах с нечетными номерами проживает больше жителей.";
+        String even = "В домах с четными номерами проживает больше жителей.";
+        System.out.println((evenNumber > oddNumber) ? even : odd);
     }
 }
---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task07.task0712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Самые-самые
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+        int countMax = 0;
+        int countMin = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        ArrayList<String> arrString = new ArrayList<String>(10);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        // fill array
+        for (int i = 0; i < arrString.size() ; i++) {
+            arrString.add(reader.readLine());
+        }
+        reader.close();
+
+        // find the longest and the shortest String
+        for (int i = 0; i < arrString.size(); i++) {
+            if(arrString.get(i).length() > max){
+                max = arrString.get(i).length();
+                countMax = i;
+            }
+
+            if(arrString.get(i).length() < min){
+                min = arrString.get(i).length();
+                countMin = i;
+            }
+        }
+
+        //find same length
+        System.out.println((countMax < countMin) ? arrString.get(countMax) : arrString.get(countMin));
+
+    }
+}
