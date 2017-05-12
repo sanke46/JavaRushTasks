82492230126a33eaf269b0be0a404e148a473177
sanke46 <shift.of.razum@gmail.com>
2017-05-12 22:47:30 +0300

    Done
    Game code :-)

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -15,6 +15,37 @@ public class Canvas {
 
     }
 
+    public void setPoint(double x, double y, char c){
+        if (!(x < 0 || y < 0 || y > matrix.length || x > matrix[0].length)){
+            int newX = Math.toIntExact(Math.round(x));
+            int newY = Math.toIntExact(Math.round(y));
+            matrix[newY][newX] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if(matrix[i][j] != 0){
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
+
+    public void clear(){
+        matrix = new char[height+2][width+2];
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     //getter
     public int getWidth() {
         return width;
