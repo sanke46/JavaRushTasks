f48b5f978acb5383f7c2e6e23bdb64e9a056e181
sanke46 <shift.of.razum@gmail.com>
2017-05-20 17:30:18 +0300

    done
    24-task 09-lesson

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by ilafedoseev on 18.05.17.
+ */
+public interface Item {
+
+    public int getId();
+    public double getPrice();
+    public String getTM();
+
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by ilafedoseev on 18.05.17.
+ */
+public interface Jeans extends Item{
+    public int getLength();
+    public int getSize();
+}
