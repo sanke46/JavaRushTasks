0295869197b3412fd3ff1615a52f66827b68d8cc
sanke46 <shift.of.razum@gmail.com>
2017-06-14 17:49:47 +0300

    2503,2502,2501 -is done :-)
    Enum Lesson

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+            String[] getwheels = loadWheelNamesFromDB();
+            wheels = new ArrayList<>();
+
+            if(getwheels.length != 4){
+                throw new IllegalArgumentException();
+            }
+
+            for(String x : getwheels){
+                wheels.add(Wheel.valueOf(x));
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
