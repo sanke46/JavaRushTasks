a1c8966ca9fc7232af13422c730636902f9f3e92
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-07-06 19:01:22 +0300

    Java chat - 3/22 tasks

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ilafedoseev on 06/07/2017.
+ */
+public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+
+    }
+
+    public static String  readString(){
+        String result = "";
+
+        while("".equals(result)){
+            try {
+                result = reader.readLine();
+            } catch (Exception e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
+
+    public static int readInt() throws IOException {
+        int result = 0;
+        while (true) {
+            try {
+                result = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by ilafedoseev on 06/07/2017.
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
