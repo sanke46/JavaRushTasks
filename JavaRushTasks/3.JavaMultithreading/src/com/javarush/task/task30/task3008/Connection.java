8947659d207e205678599c030f2f7acaae31b1d7
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-07-07 16:57:54 +0300

    Java chat - 7/22 tasks

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,7 +1,51 @@
 package com.javarush.task.task30.task3008;
 
+import javax.xml.ws.WebServiceException;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  * Created by ilafedoseev on 06/07/2017.
  */
-public class Connection {
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message m) throws IOException {
+        synchronized (out) {
+            out.writeObject(m);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in) {
+            return (Message) in.readObject();
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+    @Override
+    public void close() throws WebServiceException {
+        try {
+            socket.close();
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,50 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Handler;
+
 /**
  * Created by ilafedoseev on 06/07/2017.
  */
 public class Server {
-    public static void main(String[] args) {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper.writeMessage("Enter port number");
+        int portNumber = ConsoleHelper.readInt();
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            serverSocket.close();
+            ConsoleHelper.writeMessage("Server socket has been closed!");
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String, Connection> nameConnection : connectionMap.entrySet()) {
+            try {
+                nameConnection.getValue().send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Something has happened wtih "
+                        + nameConnection.getKey());
+            }
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
