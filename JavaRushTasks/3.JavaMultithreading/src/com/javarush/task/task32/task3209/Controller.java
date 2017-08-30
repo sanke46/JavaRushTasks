fca6c4654fd027e6dcf53f6849b1a045501a2ad4
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-08-30 22:14:01 +0300

    Done code

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/**
+ * Created by ilafedoseev on 16/08/2017.
+ */
+public class Controller {
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
+    public HTMLDocument getDocument() {
+        return document;
+    }
+
+    public void resetDocument() {
+        if(document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+            document=(HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+            document.addUndoableEditListener(view.getUndoListener());
+            view.update();
+        }
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader stringReader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(stringReader, document, document.getLength());
+        } catch (IOException e) {
+           ExceptionHandler.log(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public String getPlainText() {
+        StringWriter stringWriter = new StringWriter();
+        try {
+            new HTMLEditorKit().write(stringWriter,document,0,document.getLength());
+        } catch (IOException e) {
+            ExceptionHandler.log(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
+    }
+}
