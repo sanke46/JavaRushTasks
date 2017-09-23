90a565c6d2d573f6c139917723bc0bd88ac4c63d
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-09-23 22:03:44 +0300

    Restaurant : 2/23 level is done

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ConsoleHelper.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ConsoleHelper.java
@@ -1,4 +1,36 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return reader.readLine();
+    }
+
+    public static List<Dish> getAllDishesForOrder() throws IOException {
+        List<Dish> dishes = new ArrayList<>();
+        writeMessage(Dish.allDishesToString());
+        while (true) {
+            String dishToOrder = readString();
+            if(dishToOrder.equals("exit")) break;
+            try {
+                dishes.add(Dish.valueOf(dishToOrder));
+            } catch (IllegalArgumentException e) {
+                writeMessage(dishToOrder + " is not detected");
+            }
+        }
+        return dishes;
+    }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,13 +1,34 @@
 package com.javarush.task.task27.task2712;
 
-public class Tablet {
-    final int number;
+import com.javarush.task.task27.task2712.kitchen.Order;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Tablet extends Observable {
+    public final int number;
+    private static java.util.logging.Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
 
     public void createOrder() {
-
+        Order order = null;
+        setChanged();
+        try {
+            order = new Order(this);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
     }
+
+    @Override
+    public String toString() {
+        return String.format("Tablet{number=%d}", number);
+    }
+
 }
