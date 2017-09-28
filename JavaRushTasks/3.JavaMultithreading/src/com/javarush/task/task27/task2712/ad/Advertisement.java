424aaf7f353a4939fb8e5bfe9975cafb7810154d
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-09-28 19:37:10 +0300

    Restaurant : 8/23 level is done

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -7,11 +7,38 @@ public class Advertisement {
     private int hits;
     private int duration;
 
+    private long amountPerOneDisplaying;
+
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
         this.name = name;
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
+        amountPerOneDisplaying=initialAmount/hits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public long getAmountPerOneDisplaying() {
+        return amountPerOneDisplaying;
+    }
+
+    public void setAmountPerOneDisplaying(long amountPerOneDisplaying) {
+        this.amountPerOneDisplaying = amountPerOneDisplaying;
     }
 }
