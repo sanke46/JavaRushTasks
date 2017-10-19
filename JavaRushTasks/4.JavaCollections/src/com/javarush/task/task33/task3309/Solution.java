554ada02978504637032bbc5d5f320e244e72246
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-10-19 22:58:56 +0300

    Space 18/18 : is done

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task33.task3309;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+/*
+Комментарий внутри xml
+*/
+public class Solution {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) throws JAXBException {
+        StringWriter strWrite = new StringWriter();
+        JAXBContext context = JAXBContext.newInstance(obj.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(obj, strWrite);
+
+        return strWrite.toString();
+    }
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+    }
+}
